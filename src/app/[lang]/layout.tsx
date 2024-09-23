e8744c099460bb2de8a9a43aa8fef0fa6c1c0935
yymmm9 <yymmm9@gmail.com>
2024-09-23 23:12:47 +0200

    Update layout.tsx

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,5 @@
 import type React from "react";
 
-import { getMessages } from "next-intl/server";
 import { type Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 // import { GeistMono } from 'geist/font/mono'
